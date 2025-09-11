af5d241c1cd5626fe3b008e53be82e4ee2cafef4
Dysthesis <antheoraviel@protonmail.com>
2025-09-11 16:36:34 +1000

    Silence Nix warnings

--- nix/apps.nix
+++ nix/apps.nix
@@ -5,6 +5,9 @@ _: {
       apps.dwl = {
         type = "app";
         program = "${config.packages.dwl}/bin/dwl";
+        meta = {
+          description = "Launch dwl (wlroots-based Wayland compositor)";
+        };
       };
       apps.default = config.apps.dwl;
     };
