76f71d451501b1f68e8b3fdde98ad19d0fad11b0
Dysthesis <antheoraviel@protonmail.com>
2026-06-16 09:27:12 +1000

    build: add printf format attribute to die()
    Lets -Wformat check every die() call site.

--- util.h
+++ util.h
@@ -1,5 +1,5 @@
 /* See LICENSE.dwm file for copyright and license details. */
 
-void die(const char *fmt, ...) __attribute__((noreturn));
+void die(const char *fmt, ...) __attribute__((format(printf, 1, 2), noreturn));
 void *ecalloc(size_t nmemb, size_t size);
 int fd_set_nonblock(int fd);
